de1fb6b68a5b0d2cda0232db08c5dd9a4d8fb1d4
patrickatcw <pj8392@yahoo.com>
2018-05-10 11:33:52 -0400

    working in removal, getting error, more work required....

--- src/com/Me/Main.java
+++ src/com/Me/Main.java
@@ -1,6 +1,7 @@
 package com.Me;
 
 //http://www.java67.com/2013/02/10-examples-of-hashmap-in-java-programming-tutorial.html
+//https://www.tutorialspoint.com/java/util/hashmap_remove.htm
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,26 +12,32 @@ public class Main {
 
         HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
 
+        //(key, value)
         hashMap.put("bobcat", 7);
         hashMap.put("goat", 3);
         hashMap.put("penguin", 5);
         hashMap.put("grizzly bear", 0);
 
-        for (Map.Entry <String, Integer> entry:hashMap.entrySet()) {
+        for (Map.Entry<String, Integer> entry : hashMap.entrySet()) {
             String key = entry.getKey();
             Integer value = entry.getValue();
 
             System.out.println(key + " -----------  " + String.valueOf(value));
 
-            /*
-            grizzly bear -----------  0
+            /*grizzly bear -----------  0
             bobcat -----------  7
             goat -----------  3
-            penguin -----------  5
-             */
+            penguin -----------  5*/
+
+            hashMap.remove("bobcat");
+            System.out.println("-------------------------------------------------");
+            System.out.println("after removal of 'bobcat' ");
+            System.out.println(key + " -----------  " + hashMap);
 
 
         }
 
     }
 }
+
+
