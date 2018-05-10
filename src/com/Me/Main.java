daa8960ad51543753ca59832d0fc8614be1b0570
patrickatcw <pj8392@yahoo.com>
2018-05-10 11:13:34 -0400

    initial program created, commented results

--- src/com/Me/Main.java
+++ src/com/Me/Main.java
@@ -1,10 +1,36 @@
 package com.Me;
 
+//http://www.java67.com/2013/02/10-examples-of-hashmap-in-java-programming-tutorial.html
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+
+        hashMap.put("bobcat", 7);
+        hashMap.put("goat", 3);
+        hashMap.put("penguin", 5);
+        hashMap.put("grizzly bear", 0);
+
+        for (Map.Entry <String, Integer> entry:hashMap.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+
+            System.out.println(key + " -----------  " + String.valueOf(value));
+
+            /*
+            grizzly bear -----------  0
+            bobcat -----------  7
+            goat -----------  3
+            penguin -----------  5
+             */
+
+
+        }
+
     }
-
-
 }
